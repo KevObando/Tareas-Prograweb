534a3e65675af8921737902c1df22f49afddb457
kevin <kevin@LAPTOP-CCI5GNIG>
2022-06-06 07:12:37 -0600

    Tarea 2 semana 4

---
+++ src/test/java/com/Tareas/TareasLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
